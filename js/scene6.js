23d613fd837434ad4faf060151f3e385b956f778
pink0601 <1541056154@qq.com>
2026-06-18 14:42:38 +0800

    Update scene6.js

--- js/scene6.js
+++ js/scene6.js
@@ -80,14 +80,15 @@ function handleGamma(gamma) {
     }
 
     // 手部运动
-    // gamma < 0 (左倾): 溢出左边界 10px
-    // gamma > 0 (右倾): 向右移动，最大 20px
+    // gamma < 0 (左倾): 溢出左边界 20px
+    // gamma = 0 (水平): 溢出左边界 10px
+    // gamma > 0 (右倾): 原位
     if (gamma < 0) {
-        targetX = -10; // 左倾时溢出左边界 10px
+        targetX = -20; // 左倾时溢出左边界 20px
+    } else if (gamma === 0) {
+        targetX = -10; // 水平时溢出左边界 10px
     } else {
-        let mapped = gamma / ANGLE_LIMIT;
-        if (mapped > 1) mapped = 1;
-        targetX = mapped * 20; // 右倾时最大移动 20px
+        targetX = 0; // 右倾时原位
     }
 }
 
